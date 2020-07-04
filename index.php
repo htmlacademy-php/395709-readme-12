9cfb27e05c797c651be782dd8d4d1bfb153d37e2
Mansik18 <taktashev.m@gmail.com>
2020-07-04 18:37:20 +0600

    if

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = ''; // укажите здесь ваше имя
+$user_name = 'Мансур'; // укажите здесь ваше имя
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -40,8 +40,10 @@ $user_name = ''; // укажите здесь ваше имя
             </div>
         </form>
         <div class="header__nav-wrapper">
+            if ($is_auth==1){
             <!-- здесь должен быть PHP код, который показывает следующий тег по условию -->
             <nav class="header__nav">
+            }
                 <ul class="header__my-nav">
                     <li class="header__my-page header__my-page--popular">
                         <a class="header__page-link header__page-link--active" title="Популярный контент">
